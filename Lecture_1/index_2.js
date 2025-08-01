4196fbdf1bea012a03fc188629e40e7d29120c5b
VIkash Kumar <103258656+vikash-mdh@users.noreply.github.com>
2025-08-01 04:15:08 +0000

    this is the biggest news for  the 10  days challenga

--- Lecture_1/index_2.js
+++ Lecture_1/index_2.js
@@ -2,7 +2,8 @@ console.log("Hitesh javaScript");
 console.log("Hello vikash kumar");
 console.log("Hello deepak kumar yadav");
 
-console.log("This is the biggest javaScript playlist for the the interviews")
+console.log("This is the biggest javaScript playlist for the the interviews");
+console.log("Hello vikash kumar yadav Madhubani Bihar");
 
 
 
