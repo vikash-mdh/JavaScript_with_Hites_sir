b7bf40c3116fc05fb239c9069c5400916413d090
VIkash Kumar <103258656+vikash-mdh@users.noreply.github.com>
2025-08-01 04:06:11 +0000

    this is the biggest javaScript playlist

--- Lecture_1/index_2.js
+++ Lecture_1/index_2.js
@@ -2,5 +2,7 @@ console.log("Hitesh javaScript");
 console.log("Hello vikash kumar");
 console.log("Hello deepak kumar yadav");
 
+console.log("This is the biggest javaScript playlist for the the interviews")
+
 
 
